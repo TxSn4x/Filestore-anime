9f925f1868b2240203e2151156097c4316a858bc
TxSn4x <txsn4x@gmail.com>
2026-01-17 23:39:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/weebs_support for support")
+                self.LOGGER(__name__).info("Bot Stopped. https://t.me/weebs_support for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL3:
             try:
